bbc5a65a23d606e9fa2b092d737ae2b113a48e26
Lucas Izquierdo <lucas@MacBook-Pro.lan>
2021-06-02 03:00:40 -0400

    Added humidity feature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (lat, long, callback) =>{
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike+'. ' + 'The humidity is currently ' + body.current.humidity + '%.')
         }
     })
 }
